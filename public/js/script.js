afdc46ca484b5bee35f9f3472f1b9aa5192e2ec0
carol-sf <annacarolina.franca21@gmail.com>
2023-05-10 15:22:46 -0300

    recuperando a tarefa com jQuery

--- public/js/script.js
+++ public/js/script.js
@@ -6,3 +6,31 @@ function adicionandoTarefas() {
     btnAddTarefa.classList.toggle("esconde");
     adicaoTarefas.classList.toggle("hide");
 }
+
+// Criando as tarefas (usando o jQuery)
+
+let entrada = $("#entrada").get(0);
+let corSelecionada = "azul"; // fazer "selecionarCor()"
+$("#btn-add").on("click", adicionarTarefa);
+
+function validar() {
+    // fazer validações do vídeo
+    return true;
+}
+
+function criarTarefa(tarefa) {
+    alert("criando tarefa " + tarefa.desc);
+}
+
+function adicionarTarefa() {
+    if(validar()) {
+        let tarefa = { desc: entrada.value, cor: corSelecionada, concluida: false, arquivada: false }
+        
+        // armazenar tarefa no local storage
+
+        alert(entrada.value);
+        criarTarefa(tarefa);
+
+        // limpar formulário
+    }
+}
