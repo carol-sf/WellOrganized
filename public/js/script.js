bd0e8252e727eeebe9fe6fcf1fe630b83cb1e071
Anna Karolynna <126431104+akarolynna@users.noreply.github.com>
2023-05-10 13:36:21 -0300

    10/05/2023
    ajustando algumas partes do código

--- public/js/script.js
+++ public/js/script.js
@@ -1,8 +1,9 @@
 document.getElementById("alterarStatusDiv").addEventListener("click", adicionandoTarefas);
 const adicaoTarefas = document.getElementById("adicaoTarefas");
 const btnAddTarefa = document.getElementById("botao-add-tarefa");
-
+const tarefas = document.getElementById("tarefas");
 function adicionandoTarefas() {
     btnAddTarefa.classList.toggle("esconde");
+    tarefas.classList.toggle("esconde");
     adicaoTarefas.classList.toggle("hide");
 }
